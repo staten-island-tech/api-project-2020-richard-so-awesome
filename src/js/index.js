16501ad1da41b7d68ec29b765b10be8ed70e8105
richardso21 <richard.so21@sitechhs.com>
2020-11-13 13:20:57 -0500

    arrrgh more stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,48 @@
 // bug prevents me from doing fetches w/o this
 import "regenerator-runtime/runtime";
+
+// import variables
 import { keys } from "./keys";
 import { DOMSelectors } from "./DOM";
 
-console.log("ready");
-
 const celciusToF = (C) => C * (9 / 5) + 32;
+// simple helper to convert celcius => an obsolete measurement of temperature
 
-async function returnFetch(entry, transform, endpt) {
+async function returnFetch(entry) {
   // universal function to do fetching,
-  // transform, and give it to somewhere
+
   try {
+    // fetch the data from api
     const response = await fetch(entry);
     const data = await response.json();
-
-    const transformed = transform(data);
-
-    endpt(...transformed);
+    // return the data back
+    return data;
   } catch (error) {
+    // if error, let me know on the console
     console.log(error);
     return 1;
   }
 }
 
-function display(temp, aqi) {
+async function display(temp, aqi, placeQuery) {
+  // function for displaying the results
+  console.log(placeQuery);
+  const entry = `https://pixabay.com/api/?q=${placeQuery}&category=places&key=${keys.pixabay}`;
+  const answer = await returnFetch(entry);
+  console.log(answer);
+
+  const BG = answer.hits[0].largeImageURL;
+
+  console.log(BG);
   console.log(temp, aqi);
 }
 
-function fetchAirQuality(lat = "", long = "") {
+function fetchBG(query) {
+}
+
+async function fetchAirQuality(lat = "", long = "") {
+  // used to get AQI and Temp from current location or coordinates
+
   let entry;
   // choose api entry point according to parameter values
   if (lat === undefined || long === undefined) {
@@ -35,37 +50,35 @@ function fetchAirQuality(lat = "", long = "") {
   } else {
     entry = `https://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${long}&key=${keys.airVisual}`;
   }
-  // get answer from fetching entry
-  returnFetch(
-    entry,
-    (answer) => {
-      // get temperature + air quality index
-      const temp = celciusToF(answer.data.current.weather.tp);
-      const aqi = answer.data.current.pollution.aqius;
-      return [temp, aqi];
-    },
-    // display the result
-    display
-  );
+  // get answer from fetching entry and return to `display`
+  const answer = await returnFetch(entry);
+  console.log(answer);
+
+  // return fahrenheit temperature + air quality index
+  const temp = celciusToF(answer.data.current.weather.tp);
+  const aqi = answer.data.current.pollution.aqius;
+  const placeQuery = `${answer.data.city}`
+  // display the result
+  display(temp, aqi, placeQuery);
 }
 
 async function fetchLocation(query) {
+  // used to get the coords from a search query (airVisual api doesn't have this feature)
+
   const entry = `https://api.opencagedata.com/geocode/v1/json?q=${query}&key=${keys.openCage}`;
-  
-  // get answer from fetching entry
-  returnFetch(
-    entry,
-    // get coords of the first result from forward geocoding
-    (answer) => {
-      const geometry = answer.results[0].geometry;
-      return [geometry.lat, geometry.lng];
-    },
-    // pass to `fetchAirQuality`
-    fetchAirQuality
-  );
+
+  // get coords from openCage and return to `fetchAirQuality`
+  const answer = await returnFetch(entry);
+
+  // return coords of the first result from forward geocoding
+  const geometry = answer.results[0].geometry;
+
+  // pass to `fetchAirQuality`
+  fetchAirQuality(geometry.lat, geometry.lng);
 }
 
 function init() {
-  fetchLocation("Los Angles");
+  fetchLocation("Seattle");
+  // fetchAirQuality();
 }
 init();
